fc4d4eb26166336e36fb066910e2368b68997743
pierre <pierre.quevedo@hotmail.fr>
2022-02-02 15:43:08 +0100

    mise en place des routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgxEchartsModule } from 'ngx-echarts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
